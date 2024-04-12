cebbe4989b46d8c9b0612f8996401d02f40aaf3a
astonelv <158239756+astonelv@users.noreply.github.com>
2024-04-12 02:53:16 -0700

    complete

--- public/index.js
+++ public/index.js
@@ -3,7 +3,76 @@ async function main() {
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+    let response = await fetch("https://api.twelvedata.com/time_series?symbol=GME,%20MSFT,%20DIS,%20BNTX&interval=1min&format=JSON&apikey=c8e9db692c9144f89438584085a95e9d")
+    let result = await response.json();
+    console.log(result)
 
+    const {GME, MSFT, DIS, BNTX} = mockData;
+    const stocks = [GME, MSFT, DIS, BNTX]
+    console.log(stocks)
+
+    console.log(Chart)
+
+// Get Color Function
+    function getColor(stock){
+        if(stock === "GME"){
+            return 'rgba(61, 161, 61, 0.7)'
+        }
+        if(stock === "MSFT"){
+            return 'rgba(209, 4, 25, 0.7)'
+        }
+        if(stock === "DIS"){
+            return 'rgba(18, 4, 209, 0.7)'
+        }
+        if(stock === "BNTX"){
+            return 'rgba(166, 43, 158, 0.7)'
+        }
+    }
+    let highestPointPerStock = {}
+    let highestPricePerStock = []
+    
+    function getHighestStock(){
+        stocks.forEach(stock => {
+            if (stock && stock.values) {
+                let highValues = stock.values.map(value => parseFloat(value.high));
+                let highestForStock = Math.max(...highValues);
+                console.log(`Highest for ${stock.meta.symbol} is ${highestForStock}`)
+                highestPointPerStock[stock.meta.symbol] = highestForStock
+                highestPricePerStock.push(highestForStock)
+            }
+        });
+        return highestPricePerStock
+    }
+
+    stocks.forEach( stock => stock.values.reverse())
+    // Time Chart
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.reverse().map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.reverse().map(value => parseFloat(value.high)),
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }))
+        }
+    });
+
+    //Highest Stock Chart
+let highestPriceChart = new Chart(highestPriceChartCanvas, {
+    type: 'bar',
+    data: {
+        labels: ['GME', 'MSFT', 'DIS', 'BNTX'],
+        datasets: [{
+            label: 'Highest Stock Price',
+            data: getHighestStock(),
+            backgroundColor: stocks.map(stock => getColor(stock.meta.symbol)),
+            borderColor: stocks.map(stock => getColor(stock.meta.symbol))
+        }]
+
+    }
+});
 }
 
 main()
